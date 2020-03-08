8c495b4c2fbe8bb771f11a5ad6b25bad702be9ee
aditya-oyo <aditya1@oyorooms.com>
2020-03-08 20:02:37 +0530

    refactored services to use a common crud service. closes #11

--- pet-clinic-data/src/main/java/sharma/aditya/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/sharma/aditya/sfgpetclinic/services/PetService.java
@@ -4,8 +4,6 @@ import sharma.aditya.sfgpetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
+
 }
